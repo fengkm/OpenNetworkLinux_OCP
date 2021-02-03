da3f3a440637ce040e77dfa7cdad2bfa2c51561c
fengkm <chifeng.km@gmail.com>
2021-02-03 15:45:15 +0800

    add ufispace s9100-32x template

---
+++ packages/platforms/ufispace/x86-64/s9100-32x/onlp/builds/x86_64_ufispace_s9100_32x/module/inc/x86_64_ufispace_s9100_32x/x86_64_ufispace_s9100_32x.x
@@ -0,0 +1,14 @@
+/**************************************************************************//**
+ *
+ *
+ *
+ *****************************************************************************/
+#include <x86_64_ufispace_s9100_32x/x86_64_ufispace_s9100_32x_config.h>
+
+/* <--auto.start.xmacro(ALL).define> */
+/* <auto.end.xmacro(ALL).define> */
+
+/* <--auto.start.xenum(ALL).define> */
+/* <auto.end.xenum(ALL).define> */
+
+
---
+++ packages/platforms/ufispace/x86-64/s9100-32x/onlp/builds/x86_64_ufispace_s9100_32x/module/inc/x86_64_ufispace_s9100_32x/x86_64_ufispace_s9100_32x_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_ufispace_s9100_32x Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __X86_64_UFISPACE_S9100_32X_DOX_H__
+#define __X86_64_UFISPACE_S9100_32X_DOX_H__
+
+/**
+ * @defgroup x86_64_ufispace_s9100_32x x86_64_ufispace_s9100_32x - x86_64_ufispace_s9100_32x Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_ufispace_s9100_32x-x86_64_ufispace_s9100_32x Public Interface
+ * @defgroup x86_64_ufispace_s9100_32x-config Compile Time Configuration
+ * @defgroup x86_64_ufispace_s9100_32x-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __X86_64_UFISPACE_S9100_32X_DOX_H__ */
